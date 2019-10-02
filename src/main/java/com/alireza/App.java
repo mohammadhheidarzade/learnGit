f81f2012b88586ae6f46dfa4564c8ef77869e2ff
mohammad hosein <dr.mhheidarzade@gmail.com>
2019-10-02 16:19:30 +0330

    start the project

---
+++ src/main/java/com/alireza/App.java
@@ -0,0 +1,13 @@
+package com.alireza;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
